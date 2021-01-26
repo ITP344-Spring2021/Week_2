f89a07ff864c60190c2a0a08a7e8bbd7ca9323c1
Jack Boyuan Xu <boyuanxu@usc.edu>
2021-01-26 14:09:56 -0800

    Added programmatic UI files

---
+++ Programmatic_UI/Test/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Jack Xu on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        initUI()
+    }
+    
+    func initUI() {
+        view.backgroundColor = .systemBackground
+    }
+
+}
